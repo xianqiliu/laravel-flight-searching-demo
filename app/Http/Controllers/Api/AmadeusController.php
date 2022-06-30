a22617eb7489b0d65d52013801729b0c8a140b14
xianqiliu <xianqi.liu@eleve.isep.fr>
2022-06-30 14:25:30 +0200

    update amadeus sdk

--- app/Http/Controllers/Api/AmadeusController.php
+++ app/Http/Controllers/Api/AmadeusController.php
@@ -2,8 +2,6 @@
 
 namespace App\Http\Controllers\Api;
 
-//require_once __DIR__ . '/../../../../vendor/autoload.php';
-
 use Amadeus\Amadeus;
 use App\Http\Controllers\Controller;
 use Exception;
@@ -11,7 +9,6 @@ use Exception;
 class AmadeusController extends Controller
 {
     protected Amadeus $amadeus;
-    private ?string $bearerToken;
 
     /**
      * @throws Exception
@@ -19,26 +16,6 @@ class AmadeusController extends Controller
     public function __construct()
     {
         $this->amadeus = app(Amadeus::class);
-        $this->amadeus->getClient()->setSslCertificate(__DIR__ . '/../../../../certificate/cacert-2022-03-18.pem');
-
-        $this->bearerToken = cache('bearer_token');
-        if ($this->bearerToken) {
-            $this->amadeus->getClient()->getAccessToken()->setBearerToken($this->bearerToken);
-        } else {
-            $this->bearerToken = $this->amadeus->getClient()->getAccessToken()->getBearerToken();
-            $this->amadeus->getClient()->getAccessToken()->setBearerToken($this->bearerToken);
-            $this->cacheToken();
-        }
-
-        //var_dump($this->bearerToken);
-    }
-
-    /**
-     * @throws Exception
-     */
-    private function cacheToken()
-    {
-        cache(['bearer_token' => $this->bearerToken], now()->addSeconds(1799));
     }
 }
 
