39209bbfc2722806ae717b3e27b2b5aef2a409f7
xianqiliu <xianqi.liu@eleve.isep.fr>
2022-07-04 16:41:47 +0200

    make it better

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,7 +16,7 @@ class AppServiceProvider extends ServiceProvider
     {
         //
         $this->app->singleton(Amadeus::class, function($app) {
-            return Amadeus::builder(env('AMADEUS_CLIENT_ID'),env('AMADEUS_CLIENT_SECRET'))->build();
+            return Amadeus::builder()->build();
         });
     }
 
